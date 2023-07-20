502c232f276f9532c3af71ad5a71cdfcd188499f
vitordegf141 <vitordegf@gmail.com>
2023-07-19 23:42:23 -0300

    changed to pure c

---
+++ participants.c
@@ -0,0 +1,17 @@
+#include "strucks.c"
+
+    void AddParticipantToTable(char* Message)
+    {
+        
+        
+    }
+    
+    void UpdateParticipantStatus(char* hostname, int is_awaken)
+    {
+        
+    }
+
+    void RemoveParticipantFromTable(char* hostname)
+    {
+
+    }
