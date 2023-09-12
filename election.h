a463112201bd8b95c7cf7d8ff8d6ee266f9f1f19
vitordegf141 <57138742+vitordegf141@users.noreply.github.com>
2023-09-12 09:38:25 -0300

    Update election.h

--- election.h
+++ election.h
@@ -1,7 +1,7 @@
 #ifndef H_ELECTION
 #define H_ELECTION
 #include "participants.h"
-
+#include <errno.h>
 void * CallElection(void *arg);
 void setElectionLogFile(FILE *file);
 void *RecieveElectionMessageThread(void *arg);
